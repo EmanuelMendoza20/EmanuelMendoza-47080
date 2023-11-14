2c71d7271d46833fe959582e9942300c8535f9b7
EmanuelMendoza20 <94406081+EmanuelMendoza20@users.noreply.github.com>
2023-11-14 14:10:42 -0300

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -35,7 +35,7 @@ function calcularYMostrarResultado() {
             text: "Selecciona un peso válido ⚖️",
             duration: 3000,
             style: {
-                background: 'rgb(240, 128, 128)'
+                background: 'rgb(203, 67, 53)'
             }
             
             }).showToast();
@@ -45,10 +45,10 @@ function calcularYMostrarResultado() {
     if (!astroSeleccionado) {
         Toastify({
 
-            text: "Selecciona un astro para 🪐",
+            text: "Selecciona un astro para continuar 🪐",
             duration: 1500,
             style: {
-                background: 'rgb(240, 128, 128)'
+                background: 'rgb(203, 67, 53)',
             }
             
             }).showToast();
@@ -82,17 +82,139 @@ if (pesoAlmacenado) {
 const calcularButton = document.getElementById("calcular-button");
 calcularButton.addEventListener("click", calcularYMostrarResultado);
 
+// Stars anime
+// Twinkling Night Sky by Sharna
 
-const degradado = document.getElementById("degradado");
+// Función para generar un número aleatorio entre dos valores
+function random(min, max) {
+    return Math.random() * (max - min) + min;
+  }
+  
+  // Función para crear las estrellas fijas y agregarlas al elemento SVG
+  function createStars(num) {
+    // Obtener el elemento SVG con id="sky"
+    var sky = document.getElementById("sky");
+    // Obtener el ancho y el alto del elemento SVG
+    var vw = window.innerWidth;
+    var vh = window.innerHeight;
+    // Crear un bucle para generar las estrellas
+    for (var i = 0; i < num; i++) {
+      // Crear un elemento círculo
+      var star = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+      // Asignar un radio aleatorio entre 0.6 y 1.3
+      star.setAttribute("r", random(0.6, 1.3));
+      // Asignar una posición x aleatoria entre 0 y el ancho del elemento SVG
+      star.setAttribute("cx", random(0, vw));
+      // Asignar una posición y aleatoria entre 0 y el alto del elemento SVG
+      star.setAttribute("cy", random(0, vh));
+      // Asignar un color blanco al círculo
+      star.setAttribute("fill", "white");
+      // Asignar una clase "star" al círculo
+      star.setAttribute("class", "star");
+      // Agregar el círculo al elemento SVG
+      sky.appendChild(star);
+    }
+  }
+  
+  // Función para animar las estrellas fijas con la biblioteca anime.js
+  function animateStars() {
+    // Obtener todos los elementos con clase "star"
+    var stars = document.getElementsByClassName("star");
+    // Crear una animación con la función anime()
+    anime({
+      // Asignar los elementos con clase "star" como objetivos de la animación
+      targets: stars,
+      // Asignar una propiedad de opacidad que cambie de 0 a 1 y viceversa
+      opacity: [
+        {
+          duration: 700,
+          value: "0"
+        },
+        {
+          duration: 700,
+          value: "1"
+        }
+      ],
+      // Asignar un tipo de transición lineal
+      easing: "linear",
+      // Asignar un bucle infinito
+      loop: true,
+      // Asignar un retraso aleatorio para cada elemento
+      delay: function (el, i) {
+        return random(0, 50) * i;
+      }
+    });
+  }
+  
+  // Función para ejecutar las funciones anteriores cuando se carga la página
+  function init() {
+    // Crear 60 estrellas fijas
+    createStars(60);
+    // Animar las estrellas fijas
+    animateStars();
+  }
+  
+  // Ejecutar la función init() cuando se carga la página
+  window.onload = init;
+  
 
-// Degradado radial de fondo
-const radio = 70; // Tamaño del degradado
+// Twinkling Night Sky by Sharna
 
-// Escuchar el evento "mousemove" en el documento
-// document.addEventListener("mousemove", (e) => {
-//     const x = e.clientX / window.innerWidth;
-//     const y = e.clientY / window.innerHeight;
+const numStars = 60;
+const vw = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+const vh = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
 
-//     // Actualizar el fondo de degradado
-//     degradado.style.background = `radial-gradient(circle at ${x * 100}% ${y * 100}%, #192936, #09090B ${radio}%)`;
-// });
+const starryNight = () => {
+    anime({
+        targets: ".star",
+        opacity: [
+            { duration: 700, value: "0" },
+            { duration: 700, value: "1" }
+        ],
+        easing: "linear",
+        loop: true,
+        delay: (el, i) => 50 * i
+    });
+};
+
+const shootingStars = () => {
+    anime({
+        targets: ".wish",
+        easing: "linear",
+        loop: true,
+        delay: (el, i) => 1000 * i,
+        opacity: [{ duration: 700, value: "1" }],
+        width: [
+            { value: "150px" },
+            { value: "0px" }
+        ],
+        translateX: 350
+    });
+};
+
+const randomRadius = () => Math.random() * 0.7 + 0.6;
+const getRandomX = () => Math.floor(Math.random() * vw).toString();
+const getRandomY = () => Math.floor(Math.random() * vh).toString();
+
+document.addEventListener("DOMContentLoaded", function () {
+    const sky = document.getElementById("App");
+    const skyContent = [];
+
+    for (let i = 0; i < numStars; i++) {
+        skyContent.push(`<circle cx="${getRandomX()}" cy="${getRandomY()}" r="${randomRadius()}" stroke="none" strokeWidth="0" fill="#" class="star"></circle>`);
+    }
+
+    sky.innerHTML = `
+        <svg id="sky" width="${vw}" height="${vh}" style="position: absolute; top: 0; left: 0;">
+            ${skyContent.join('')}
+        </svg>
+        <div id="shootingstars" style="position: absolute; top: 0; left: 0;">
+            ${[...Array(60)].map(() => `
+                <div class="wish" style="left: ${getRandomY()}px; top: ${getRandomX()}px;"></div>
+            `).join('')}
+        </div>
+    `;
+
+    starryNight();
+    shootingStars();
+});
